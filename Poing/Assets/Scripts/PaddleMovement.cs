9338c9e75949006bc4befb5a9e87ea49da5dd732
RohanPillaniwalla <58022750+RohanPillaniwalla@users.noreply.github.com>
2022-08-01 01:56:50 +0530

    updated paddle movement script

--- Poing/Assets/Scripts/PaddleMovement.cs
+++ Poing/Assets/Scripts/PaddleMovement.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PaddleMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float speed = 5f;
+    private float movement;
+
+    public Rigidbody2D rb;
+    public bool isPlayer1;
+   
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(isPlayer1)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+
+        rb.velocity = new Vector2(rb.velocity.x, speed * movement);
     }
 }
