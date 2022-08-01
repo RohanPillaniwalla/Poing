12bc455b567a259edbac55137e4c61086894cec2
RohanPillaniwalla <58022750+RohanPillaniwalla@users.noreply.github.com>
2022-08-01 13:12:05 +0530

    Added BallMovement script to ball

---
+++ Poing/Assets/Scripts/BallMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    private float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //calling the BallLauncher
+        BallLauncher();
+    }
+
+    //Launching the ball in random direction
+    private void BallLauncher ()
+    {
+        float x = Random.Range(0, 2) == 0 ? -1 : 1;     //setting value of x between -1 and 1
+        float y = Random.Range(0, 2) == 0 ? -1 : 1;     //setting value of x between -1 and 1
+
+        //giving velocity to ball in certain direction
+        rb.velocity = new Vector2(speed * x, speed * y);
+    }
+}
